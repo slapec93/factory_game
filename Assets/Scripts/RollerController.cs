d43dd36f2f5cb2bbbce57b8a4392a4e2b6e14d42
Gergely Bekesi <gergely.bekesi@bitrise.io>
2018-11-25 18:58:45 +0100

    Create grids

---
+++ Assets/Scripts/RollerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollerController : MonoBehaviour {
+
+	Animator animator;
+	bool isRolling = false;
+	float rollingSpeed;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+		animator.speed = rollingSpeed;
+		ToggleRolling();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void ToggleRolling() {
+		isRolling = !isRolling;
+		animator.SetBool("IsRolling", isRolling);
+	}
+
+	public void SetSpeed(float newSpeed) {
+		rollingSpeed = newSpeed;
+	}
+}
